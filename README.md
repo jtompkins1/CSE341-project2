f9bfaa09b8ab518275969a167697c721d6ec0109
jtompkins1 <jennifermtompkins@gmail.com>
2026-01-18 22:57:03 -0600

    connected to database

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Week 3-4 project
 
 How to start a new node.js project: https://byui-cse.github.io/cse341-ww-course/week01/project.html
+Then create a collection in MongoDB
+Store your mongoDB connection string in teh .env file.
--- server.js
+++ server.js
@@ -1,12 +1,26 @@
+//server.js
+
 const express = require('express');
+
+const mongodb = require('./data/database');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+mongodb.initDb((err) => {
+  if (err) {
+    console.log('Unable to connect to database', err);
+  } 
+  else {
+    app.listen(port, () => {
+      console.log(`Database is listening and node is running on port ${port}`)
+    });
+  }
 });
+
+
+module.exports = mongodb;
 
 module.exports = app;
